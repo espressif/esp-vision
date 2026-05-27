2c58473da1aaea76b606e607a45c88d64ae7b779
yanke <yanke2@espressif.com>
2026-05-27 20:32:21 +0800

    feat: make default filesystem scripts board-owned

--- boards/TEMPLATE/manifest.py
+++ boards/TEMPLATE/manifest.py
@@ -3,4 +3,5 @@
 
 freeze("$(PORT_DIR)/modules")
 freeze("$(ESP_VISION_ROOT)/modules", "py_inisetup.py")
+freeze("$(ESP_VISION_ROOT)/boards/TEMPLATE", "board_inisetup.py")
 include("$(MPY_DIR)/extmod/asyncio")
